d861eaeb1978438ffe8508ad5d929eb8d2b3e3ae
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2011-07-21 14:04:06 +0900

    local dictionary

--- server.rb
+++ server.rb
@@ -5,23 +5,49 @@
 require 'rubygems'
 require 'sinatra'
 require 'fast_stemmer' #for stem
-require './dic-session'
+# require './dic-session' # ローカル辞書にしてイースト辞書webサービスは使わないようにした。
+
+DIC_FILE = 'PrepTutorEJDIC/PrepTutorEJDIC.UTF-8.txt'
+
+def search(word, condition = 'exact')
+  if condition == 'beginwith' then
+    reg = Regexp.new("^#{word}")
+  else
+    reg = Regexp.new("^#{word}\\t")
+  end
+    
+  result = []
+  File.open(DIC_FILE) { |f|
+    until f.eof?
+      line = f.gets
+      line.sub!(/\n/, '');
+      result.insert(-1, line.sub(/\t/, '\n')) if reg =~ line
+      # ここで単語の後のtabを改行に換える整形もしておく。
+    end
+  }
+
+  return result
+end
 
 get '/dic' do
+  word = params[:Word]
+
   if params[:_callback]
     content_type 'text/javascript', :charset => 'utf-8'
   else
     content_type 'text/json', :charset => 'utf-8'
   end
 
-  session = DicSession.new
-  ids = session.search(params[:Word], 'EXACT')
-  ids = session.search(params[:Word].stem, 'EXACT') if ids.empty?
-  ids = session.search(params[:Word].stem, 'STARTWITH') if ids.empty?
+  result = search(word)
+  if result.empty? then
+    stem = word.stem
+    result = search(stem)
+    if result.empty? then
+      result = search(stem, 'beginwith')
+    end
+  end
 
-  result = ids.inject('') { |m, e| m += session.retrieve(e) }
-  session.close
-
+  result = result.join('\n')
   if params[:_callback] then
     params[:_callback] + '("' + result + '");'
   else
