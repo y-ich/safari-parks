5f60a24fad0515103144b56ab7c769bea35abcfb
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2011-07-27 14:19:23 +0900

    add twitter bot function

---
+++ repeat_bot.rb
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+require 'twitter'
+require 'monitor'
+
+# 「自然に学習」 on nextliteracy
+CONSUMER_KEY = 'uJc6bLJMjoA2dJ1xnkhvyw'
+CONSUMER_SECRET = 'Z10pLyTGuZVbYEbr2MHqkyUIQo3wkQDDROjMYOWbo'
+# repeat_botの認証
+ACCESS_TOKEN = '343043759-NtkQE7odf9T26eTB6vaa07keyaSSMaU8YhuvUBd4'
+ACCESS_TOKEN_SECRET = '22qnX1llOsuzwb2gogcOQ0dWfO1lTak84JAH4u7CkE'
+
+Twitter.configure do |config|
+  config.consumer_key = CONSUMER_KEY
+  config.consumer_secret = CONSUMER_SECRET
+  config.oauth_token = ACCESS_TOKEN
+  config.oauth_token_secret = ACCESS_TOKEN_SECRET
+end
+
+class Repeat_bot
+  include MonitorMixin
+
+  def initialize
+    super()
+    @schedule = []
+    @thread = nil
+    @client = Twitter::Client.new
+  end
+
+  def << task
+    synchronize do
+      @schedule << task
+    end
+    @thread = Thread.new {run} if @thread.nil? or not @thread.alive?
+  end
+
+  def push(*tasks)
+    synchronize do
+      @schedule.push(*tasks)
+    end
+    @thread = Thread.new {run} if @thread.nil? or not @thread.alive?
+  end
+
+  def run
+    until synchronize {@schedule.empty?}
+      task = synchronize do
+        @schedule = @schedule.sort
+        @schedule.delete_at(0)
+      end
+      t = task[0] - Time.now
+      sleep(t) if t > 0
+      tweet = '@' + task[1] + ' ' + task[2]
+      @client.update(tweet[0,140])
+    end
+  end
+end
+
+
--- server.rb
+++ server.rb
@@ -2,15 +2,16 @@
 #
 # Bookmarklet連携CGIサーバー
 #
-require 'rubygems'
 require 'sinatra'
 require 'fast_stemmer' #for stem
 # require './dic-session' # ローカル辞書にしてイースト辞書webサービスは使わないようにした。
+require './repeat_bot'
 
 DIC_FILE = 'PrepTutorEJDIC/PrepTutorEJDIC.UTF-8.txt'
+repeat_bot = Repeat_bot.new
 
 def search(word, condition = 'exact')
-  if condition == 'beginwith' then
+  if condition == 'beginwith'
     reg = Regexp.new("^#{word}", true)
   else
     reg = Regexp.new("^#{word}\\t", true)
@@ -33,15 +34,24 @@ get '/dic' do
   word = params[:Word]
 
   result = search(word)
-  if result.empty? then
+  if result.empty?
     stem = word.stem
     result = search(stem)
-    if result.empty? then
+    if result.empty?
       result = search(stem, 'beginwith')
     end
   end
 
   result = result.join('\n')
+
+  if params[:twitter_id]
+    t = Time.now
+    repeat_bot << [t + 60*60, params[:twitter_id], result]
+    repeat_bot << [t + 60*60*24, params[:twitter_id], result]
+    repeat_bot << [t + 60*60*24*7, params[:twitter_id], result]
+    repeat_bot << [t + 60*60*24*30, params[:twitter_id], result]
+  end
+
   if params[:_callback]
     content_type('text/javascript', :charset => 'utf-8')
     params[:_callback] + '("' + result + '");'
@@ -50,3 +60,4 @@ get '/dic' do
     '"' + result + '"'
   end
 end
+
