bd4e37cc879c2de94bfedecacc0b874b9d269e44
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2011-08-03 16:42:18 +0900

    treated multi-frame.

--- bookmarklets/dic.js
+++ bookmarklets/dic.js
@@ -7,12 +7,18 @@ javascript:{
 	alert(((j.length == 0) ? w + 'は見つかりませんでした' : j))
     }
     if (w.length == 0) {
-	w = prompt('単語を入力');
-	if (w == null) return
+	var fs = window.frames;
+	for (var i = 0; (i < fs.length) && (w.length == 0); i++) {
+	    w = fs[i].getSelection().toString();
+	}
+	if (w.length == 0) {
+	    w = prompt('単語を入力');
+	    if (w == null) return
+	}
     }
     s.type = 'text/javascript';
     s.src = 'http://safari-parks.heroku.com/dic?Word=' +
 	w.replace(/^\s+/, '').replace(/\s+$/, '') + 
-	'&_callback=cb';
+	'&_callback=cb&twitter_id=y_ich';
     d.body.appendChild(s)
 }
