036b69362c7e4d1aac01584a3cd1d3c013326098
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2011-08-03 15:32:52 +0900

    used JSMin. reset author, again.

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'delayed/tasks'
+require 'jsmin'
 require './server.rb'
 
 task :environment
@@ -9,5 +10,13 @@ namespace :db do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
     ActiveRecord::Migration.verbose = true
     ActiveRecord::Migrator.migrate("db/migrate")
+  end
+end
+
+file "bookmarklets/dic.min.js" => "bookmarklets/dic.js" do |t|
+  File.open(t.prerequisites[0], 'r') do |orig|
+    File.open(t.name, 'w') do |min|
+      min.puts JSMin.minify(orig)
+    end
   end
 end
--- bookmarklets/dic.js
+++ bookmarklets/dic.js
@@ -1,1 +1,18 @@
-javascript:{var d=document;var w=d.getSelection().toString();var s=d.createElement('script');function cb(j){d.body.removeChild(s);alert(((j.length==0)?w+'は見つかりませんでした':j))}if(w.length==0){w=prompt('単語を入力');if(w==null)return}s.type='text/javascript';s.src='http://safari-parks.heroku.com/dic?Word='+w.replace(/^\s+/, '').replace(/\s+$/, '')+'&_callback=cb';d.body.appendChild(s)}
+javascript:{
+    var d = document;
+    var w = d.getSelection().toString();
+    var s = d.createElement('script');
+    function cb(j) {
+	d.body.removeChild(s);
+	alert(((j.length == 0) ? w + 'は見つかりませんでした' : j))
+    }
+    if (w.length == 0) {
+	w = prompt('単語を入力');
+	if (w == null) return
+    }
+    s.type = 'text/javascript';
+    s.src = 'http://safari-parks.heroku.com/dic?Word=' +
+	w.replace(/^\s+/, '').replace(/\s+$/, '') + 
+	'&_callback=cb';
+    d.body.appendChild(s)
+}
