6b5748c995cea3fd507490e23beb07da4ebf2382
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2011-08-04 22:58:27 +0900

    translated database.yml as ERB because heroku's database.yml seems ERB.

--- server.rb
+++ server.rb
@@ -14,7 +14,7 @@ require 'delayed_job'
 require 'twitter'
 
 configure do
-  config = YAML::load(File.open('config/database.yml'))
+  config = YAML::load(ERB.new(File.open('config/database.yml')))
   environment = Sinatra::Application.environment.to_s
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   ActiveRecord::Base.establish_connection(config[environment])
