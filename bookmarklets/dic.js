ab16d4edb8b292dcc922e99fd2b379c8ad6cf286
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2011-08-05 10:46:57 +0900

    treated frame with URL.

--- bookmarklets/dic.js
+++ bookmarklets/dic.js
@@ -4,21 +4,22 @@ javascript:{
     var s = d.createElement('script');
     function cb(j) {
 	d.body.removeChild(s);
-	alert(((j.length == 0) ? w + 'は見つかりませんでした' : j))
+	alert(((j.length == 0) ? w + 'は見つかりませんでした' : j));
     }
     if (w.length == 0) {
 	var fs = window.frames;
 	for (var i = 0; (i < fs.length) && (w.length == 0); i++) {
-	    w = fs[i].getSelection().toString();
+	    if (fs[i].getSelection != null)
+		w = fs[i].getSelection().toString();
 	}
-	if (w.length == 0) {
-	    w = prompt('単語を入力');
-	    if (w == null) return
-	}
+	console.log(w.length);
+	if (w.length == 0) w = prompt('単語を入力');
     }
-    s.type = 'text/javascript';
-    s.src = 'http://safari-park.heroku.com/dic/search?Word=' +
-	w.replace(/^\s+/, '').replace(/\s+$/, '') + 
-	'&_callback=cb&twitter_id=<screen_name>';
-    d.body.appendChild(s)
+    if (w != null) {
+	s.type = 'text/javascript';
+	s.src = 'http://safari-park.heroku.com/dic/search?Word=' +
+	    w.replace(/^\s+/, '').replace(/\s+$/, '') + 
+	    '&_callback=cb&twitter_id=<screen_name>';
+	d.body.appendChild(s);
+    }
 }
