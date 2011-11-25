4808107c1c4dc7d6589e223cd6efb11483dfa013
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2011-11-25 22:17:17 +0900

    improved iphone version.

--- bookmarklets/dic_iphone.js
+++ bookmarklets/dic_iphone.js
@@ -18,25 +18,36 @@ javascript:{
 	e.style.top = (wi.pageYOffset + wi.innerHeight - 24) + 'px';
 	e.style.left = (wi.pageXOffset + wi.innerWidth - 56) + 'px';
     e.appendChild(d.createTextNode('E->J'));
-    e.onclick = function () {
-		var w = d.getSelection().toString();
-		var s = d.createElement('script');
-		if (w.length == 0) {
-			var fs = window.frames;
-			for (var i = 0; (i < fs.length) && (w.length == 0); i++) {
-				if (fs[i].getSelection != null)
-					w = fs[i].getSelection().toString();
+	e.ontouchstart = function () {
+		wi.sp_t = new Date().getTime();
+		wi.sp_w = d.getSelection().toString();
+	};
+    e.ontouchend = function () {
+		if (new Date().getTime() - wi.sp_t < 500) {
+			var w = wi.sp_w;
+			var s = d.createElement('script');
+			if (w.length == 0) {
+				var fs = window.frames;
+				for (var i = 0; (i < fs.length) && (w.length == 0); i++) {
+					if (fs[i].getSelection != null)
+						w = fs[i].getSelection().toString();
+				}
+				if (w.length == 0) w = prompt('単語を入力');
 			}
-			if (w.length == 0) w = prompt('単語を入力');
+			if (w != null) {
+				s.id = 'dic_jsonp';
+				s.type = 'text/javascript';
+				s.src = 'SERVER_DOMAIN/dic/search?Word=' +
+					w.replace(/^\s+/, '').replace(/\s+$/, '') + 
+					'&_callback=cb&twitter_id=&v=2';
+				d.body.appendChild(s);
+			}
 		}
-		if (w != null) {
-			s.id = 'dic_jsonp';
-			s.type = 'text/javascript';
-			s.src = 'SERVER_DOMAIN/dic/search?Word=' +
-				w.replace(/^\s+/, '').replace(/\s+$/, '') + 
-				'&_callback=cb&twitter_id=&v=2';
-			d.body.appendChild(s);
-		}
+		else {
+			if (typeof window.sp_pronounce !== 'undefined' && window.sp_pronounce !== null)
+				(new Audio(window.sp_pronounce)).play();
+			else alert('先に単語を調べてください。');
+		}			
     };
     d.body.appendChild(e);
     wi.addEventListener('scroll', function () {
